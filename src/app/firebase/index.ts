b2cd8ca9837367310d7b1486542e5d2a55fb3a5c
thinceller <kohei.ct@icloud.com>
2019-03-11 05:51:23 +0900

    Set firebase init

---
+++ src/app/firebase/index.ts
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+};
+
+const fire = !firebase.apps.length
+  ? firebase.initializeApp(config)
+  : firebase.app();
+
+export const auth = fire.auth();
--- src/app/store/auth/actions.ts
+++ src/app/store/auth/actions.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import { auth } from '../../firebase';
 
 export const actionTypes = {
   createUser: 'CREATE_USER',
@@ -6,49 +6,49 @@ export const actionTypes = {
   logoutUser: 'LOGOUT_USER',
 };
 
-export function createUser(email: string, password: string) {
+export const createUser = (email: string, password: string) => dispatch => {
   return dispatch => {
-    firebase.auth()
-            .createUserWithEmailAndPassword(email, password)
-            .then(res => {
-              const newUser = res.user;
-              if (!newUser) { return; }
-              const user = {
-                uid: newUser.uid,
-                email: newUser.email,
-                providerId: newUser.providerId,
-              };
-              dispatch(actionTypes.createUser, { payload: user });
-            });
+    auth.createUserWithEmailAndPassword(email, password)
+          .then(res => {
+            console.log(res);
+            const newUser = res.user;
+            if (!newUser) { return; }
+            const user = {
+              uid: newUser.uid,
+              email: newUser.email,
+              providerId: newUser.providerId,
+            };
+            dispatch(actionTypes.createUser, { payload: user });
+          })
+          .catch(err => console.error(err.message));
   };
-}
+};
 
-export function loginUser(email: string, password: string) {
+export const loginUser = (email: string, password: string) => dispatch => {
+  console.log(email, password);
   return dispatch => {
-    firebase.auth()
-            .signInWithEmailAndPassword(email, password)
-            .then(res => {
-              const currentUser = res.user;
-              if (!currentUser) { return; }
-              const user = {
-                uid: currentUser.uid,
-                displayNaame: currentUser.displayName,
-                email: currentUser.email,
-                emailVerified: currentUser.emailVerified,
-                photoURL: currentUser.photoURL,
-                providerId: currentUser.providerId,
-              };
-              dispatch(actionTypes.loginUser, { payload: user });
-            });
+    auth.signInWithEmailAndPassword(email, password)
+          .then(res => {
+            const currentUser = res.user;
+            if (!currentUser) { return; }
+            const user = {
+              uid: currentUser.uid,
+              displayNaame: currentUser.displayName,
+              email: currentUser.email,
+              emailVerified: currentUser.emailVerified,
+              photoURL: currentUser.photoURL,
+              providerId: currentUser.providerId,
+            };
+            dispatch(actionTypes.loginUser, { payload: user });
+          });
   };
-}
+};
 
 export function logoutUser() {
   return dispatch => {
-    firebase.auth()
-            .signOut()
-            .then(() => {
-              dispatch(actionTypes.logoutUser, { payload: null });
-            });
+    auth.signOut()
+          .then(() => {
+            dispatch(actionTypes.logoutUser, { payload: null });
+          });
   };
 }
