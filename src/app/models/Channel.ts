01795890f629171f5d3f7f529aa766d92ff63392
thinceller <kohei.ct@icloud.com>
2019-03-25 09:03:47 +0900

    Fix Channel model

--- src/app/models/Channel.ts
+++ src/app/models/Channel.ts
@@ -2,12 +2,22 @@ export default class Channel {
   constructor(
     public channelId: string,
     public channelName: string,
+    public channelImage: string,
   ) {}
 
   static createEmpty(): Channel {
     return new Channel(
       '',
       '',
+      '',
+    );
+  }
+
+  clone(): Channel {
+    return new Channel(
+      this.channelId,
+      this.channelName,
+      this.channelImage,
     );
   }
 }
